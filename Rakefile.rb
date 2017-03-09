ab0145a50e9782be76cc8440135eb721a0f2101b
kexibuaijifen <137311107@qq.com>
2017-03-09 21:02:42 +0800

    ubuntu部分修改了

---
+++ Rakefile.rb
@@ -0,0 +1,79 @@
+require 'json'
+PROJECT_DIR='/paas-agent-omnibus'
+
+namespace :agent do
+  desc 'Cleanup generated files'
+  task :clean do
+    puts "Clean up generated files"
+    sh "rm -rf /var/cache/omnibus/pkg/*"
+    sh "rm -f /etc/init.d/datadog-agent"
+    sh "rm -rf /etc/paas-agent"
+    sh "rm -rf /opt/paas-agent"
+  end
+
+  desc 'Pull the integrations repo'
+  task :'pull-integrations' do
+    integration_branch = ENV['VERSION'] || 'master'
+
+    sh "rm -rf /#{ENV['INTEGRATIONS_REPO']}"
+    sh "git clone https://github.com/DataDog/#{ENV['INTEGRATIONS_REPO']}.git /#{ENV['INTEGRATIONS_REPO']} || true"
+    sh "cd /integrations-core && ls -a && git status"
+    sh "cd /#{ENV['INTEGRATIONS_REPO']} && git fetch --all"
+    sh "cd /#{ENV['INTEGRATIONS_REPO']} && git checkout dd-check-#{ENV['INTEGRATION']}-#{integration_branch} ||
+        git checkout #{integration_branch}"
+    sh "cd /#{ENV['INTEGRATIONS_REPO']} && git reset --hard"
+  end
+
+  desc 'Execute script'
+  task :'execute-script' do
+    sh "cd #{PROJECT_DIR} && bundle update"
+    puts "building integration #{ENV['INTEGRATION']}"
+
+    manifest = JSON.parse(File.read("/#{ENV['INTEGRATIONS_REPO']}/#{ENV['INTEGRATION']}/manifest.json"))
+    # The manifest should always have a version
+    integration_version = manifest['version']
+
+    header = erb_header({
+      'name' => "#{ENV['INTEGRATION']}",
+      'version' => "#{integration_version}",
+      'build_iteration' => "#{ENV['BUILD_ITERATION']}"
+    })
+    sh "(echo '#{header}' && cat #{PROJECT_DIR}/resources/datadog-integrations/project.rb.erb) | erb > #{PROJECT_DIR}/config/projects/dd-check-#{ENV['INTEGRATION']}.rb"
+
+    header = erb_header({
+      'name' => "#{ENV['INTEGRATION']}",
+      'PROJECT_DIR' => "#{PROJECT_DIR}",
+      'integrations_repo' => "#{ENV['INTEGRATIONS_REPO']}"
+    })
+    sh "(echo '#{header}' && cat #{PROJECT_DIR}/resources/datadog-integrations/software.rb.erb) | erb > #{PROJECT_DIR}/config/software/dd-check-#{ENV['INTEGRATION']}-software.rb"
+
+    sh "cd #{PROJECT_DIR} && bin/omnibus build dd-check-#{ENV['INTEGRATION']} --output_manifest=false"
+  end
+
+  desc 'Build an integration'
+  task :'build-integration' do
+    Rake::Task["agent:clean"].invoke
+    Rake::Task["env:import-rpm-key"].invoke
+    Rake::Task["agent:pull-integrations"].invoke
+    Rake::Task["agent:execute-script"].invoke
+  end
+
+end
+
+namespace :env do
+  desc 'Import signing RPM key'
+  task :'import-rpm-key' do
+    # If an RPM_SIGNING_PASSPHRASE has been passed, let's import the signing key
+    sh "if [ \"$RPM_SIGNING_PASSPHRASE\" ]; then gpg --import /keys/RPM-SIGNING-KEY.private; fi"
+  end
+end
+
+def erb_header(variables)
+  # ERB does not support setting template variables on the command line
+  # this method generates a header usable by a ERB file
+  out = ""
+  variables.each do |key, value|
+    out += "<% #{key}=\"#{value}\" %>"
+  end
+  out
+end
---
+++ config/projects/paas-agent.rb
@@ -0,0 +1,190 @@
+require "./lib/ostools.rb"
+
+name 'paas-agent'
+maintainer 'PaasInsight Packages <package@paasinsight.com>'
+homepage 'http://www.paasinsight.com'
+install_dir '/opt/paas-agent'
+
+#build_version do
+  #source :git, from_dependency: 'datadog-agent'
+  #output_format :dd_agent_format
+#end
+build_version   Omnibus::BuildVersion.new.semver
+
+build_iteration 1
+
+description 'PassInsight Monitoring Agent
+ The PaasInsight Monitoring Agent is a lightweight process that monitors system
+ processes and services, and sends information back to your PaasInsight account.
+ .
+ This package installs and runs the advanced Agent daemon, which queues and
+ forwards metrics from your applications as well as system services.
+ .
+ See http://www.paasinsight.com/ for more information
+'
+
+# ------------------------------------
+# Generic package information
+# ------------------------------------
+
+# .deb specific flags
+package :deb do
+  vendor 'PaasInsight <info@paasinsight.com>'
+  epoch 1
+  license 'Simplified BSD License'
+  section 'utils'
+  priority 'extra'
+end
+
+# .rpm specific flags
+package :rpm do
+  vendor 'PaasInsight <package@paasinsight.com>'
+  epoch 1
+  dist_tag ''
+  license 'Simplified BSD License'
+  category 'System Environment/Daemons'
+  priority 'extra'
+  if ENV.has_key?('RPM_SIGNING_PASSPHRASE') and not ENV['RPM_SIGNING_PASSPHRASE'].empty?
+    signing_passphrase "#{ENV['RPM_SIGNING_PASSPHRASE']}"
+  end
+end
+
+if redhat?
+  runtime_dependency 'initscripts'
+end
+
+# OSX .pkg specific flags
+package :pkg do
+  identifier 'com.paasinsight.agent'
+  signing_identity 'Developer ID Installer: PaasInsight, Inc. (JKFCB4CN7C)'
+end
+compress :dmg do
+  window_bounds '200, 200, 750, 600'
+  pkg_position '10, 10'
+end
+
+# Note: this is to try to avoid issues when upgrading from an
+# old version of the agent which shipped also a datadog-agent-base
+# package.
+if redhat?
+  replace 'datadog-agent-base < 5.0.0'
+  replace 'datadog-agent-lib < 5.0.0'
+elsif debian?
+  replace 'datadog-agent-base (<< 5.0.0)'
+  replace 'datadog-agent-lib (<< 5.0.0)'
+  conflict 'datadog-agent-base (<< 5.0.0)'
+end
+
+# ------------------------------------
+# OS specific DSLs and dependencies
+# ------------------------------------
+
+# Linux
+if linux?
+  # Debian
+  if debian?
+    extra_package_file '/etc/init.d/paas-agent'
+    extra_package_file '/lib/systemd/system/paas-agent.service'
+  end
+
+  # SysVInit service file
+  if redhat?
+    extra_package_file '/etc/rc.d/init.d/paas-agent'
+  end
+
+  if suse?
+    extra_package_file '/etc/init.d/paas-agent'
+    extra_package_file '/usr/lib/systemd/system/paas-agent.service'
+  end
+
+  # Supervisord config file for the agent
+  extra_package_file '/etc/paas-agent/supervisor.conf'
+
+  # Example configuration files for the agent and the checks
+  extra_package_file '/etc/paas-agent/paasinsight.conf.example'
+  extra_package_file '/etc/paas-agent/conf.d'
+
+  # Custom checks directory
+  extra_package_file '/etc/paas-agent/checks.d'
+
+  # Just a dummy file that needs to be in the RPM package list if we don't want it to be removed
+  # during RPM upgrades. (the old files from the RPM file listthat are not in the new RPM file
+  # list will get removed, that's why we need this one here)
+  extra_package_file '/usr/bin/paas-agent'
+
+  # Linux-specific dependencies
+  dependency 'procps-ng'
+  dependency 'sysstat'
+end
+
+# Mac and Windows
+if osx? or windows?
+  dependency 'gui'
+end
+
+# ------------------------------------
+# Dependencies
+# ------------------------------------
+
+# creates required build directories
+dependency 'preparation'
+
+# Agent dependencies
+dependency 'boto'
+dependency 'docker-py'
+dependency 'ntplib'
+dependency 'protobuf-py'
+dependency 'pycrypto'
+dependency 'pyopenssl'
+dependency 'python-consul'
+dependency 'python-etcd'
+dependency 'pyyaml'
+dependency 'simplejson'
+dependency 'supervisor'
+dependency 'tornado'
+dependency 'uptime'
+dependency 'uuid'
+dependency 'zlib'
+
+# Check dependencies
+dependency 'adodbapi'
+dependency 'beautifulsoup4'
+dependency 'dnspython'
+dependency 'httplib2'
+dependency 'kafka-python'
+dependency 'kazoo'
+dependency 'paramiko'
+dependency 'pg8000'
+dependency 'psutil'
+dependency 'psycopg2'
+dependency 'pymongo'
+dependency 'pymysql'
+dependency 'pysnmp'
+dependency 'python-gearman'
+dependency 'python-memcached'
+dependency 'python-redis'
+dependency 'python-rrdtool'
+dependency 'pyvmomi'
+dependency 'requests'
+dependency 'scandir'
+dependency 'snakebite'
+
+# Additional software
+dependency 'datadogpy'
+
+# datadog-gohai and datadog-metro are built last before datadog-agent since they should always
+# be rebuilt (if put above, they would dirty the cache of the dependencies below
+# and trigger a useless rebuild of many packages)
+dependency 'datadog-gohai'
+if linux? and ohai['kernel']['machine'] == 'x86_64'
+  dependency 'datadog-metro'
+end
+
+#PaasInsight agent
+dependency 'paas-agent'
+
+# version manifest file
+dependency 'version-manifest'
+
+exclude '\.git*'
+exclude 'bundler\/git'
