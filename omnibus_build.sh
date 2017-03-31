095c6a8433d34a6eae0d0d6a1cf8b25966a9c370
kexibuaijifen <dmwan_0926@126.com>
2017-03-31 14:16:30 +0800

    Update omnibus_build.sh

--- omnibus_build.sh
+++ omnibus_build.sh
@@ -23,24 +23,6 @@ rm -f /etc/init.d/paas-agent
 rm -rf /etc/paas-agent
 rm -rf /opt/$PROJECT_NAME/*
 
-cd $PROJECT_DIR
-# Allow to use a different dd-agent-omnibus branch
-git fetch --all
-git checkout $OMNIBUS_BRANCH
-git reset --hard origin/$OMNIBUS_BRANCH
-
-# If an RPM_SIGNING_PASSPHRASE has been passed, let's import the signing key
-if [ -n "$RPM_SIGNING_PASSPHRASE" ]; then
-  gpg --import /keys/RPM-SIGNING-KEY.private
-fi
-
-# Last but not least, let's make sure that we rebuild the agent everytime because
-# the extra package files are destroyed when the build container stops (we have
-# to tweak omnibus-git-cache directly for that). Same for gohai and go-metro.
-git --git-dir=/var/cache/omnibus/cache/git_cache/opt/datadog-agent tag -d `git --git-dir=/var/cache/omnibus/cache/git_cache/opt/datadog-agent tag -l | grep datadog-agent` || true
-git --git-dir=/var/cache/omnibus/cache/git_cache/opt/datadog-agent tag -d `git --git-dir=/var/cache/omnibus/cache/git_cache/opt/datadog-agent tag -l | grep datadog-gohai` || true
-git --git-dir=/var/cache/omnibus/cache/git_cache/opt/datadog-agent tag -d `git --git-dir=/var/cache/omnibus/cache/git_cache/opt/datadog-agent tag -l | grep datadog-metro` || true
-
 # Install the gems we need, with stubs in bin/
 bundle update # Make sure to update to the latest version of omnibus-software
 bin/omnibus build -l=$LOG_LEVEL $PROJECT_NAME
